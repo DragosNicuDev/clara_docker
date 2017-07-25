d5ff46268d18f7dc592557b369b5c537ecd5c925
Dragos Nicu <contact@dragosnicu.com>
2017-07-25 14:49:19 +0100

    Modified settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -260,7 +260,7 @@ LOGIN_REDIRECT_URL = '/'
 
 
 # Location of root django.contrib.admin URL, use {% url 'admin:index' %}
-ADMIN_URL = r'^admin/'
+#ADMIN_URL = r'^admin/'
 
 # Your common stuff: Below this line define 3rd party library settings
 # ------------------------------------------------------------------------------

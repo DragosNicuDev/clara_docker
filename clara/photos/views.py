d2d00b8de90198369601f3f5436c742419507e4f
Dragos Nicu <contact@dragosnicu.com>
2017-07-26 15:27:41 +0100

    Refactor the upload app

--- clara/photos/views.py
+++ clara/photos/views.py
@@ -8,11 +8,30 @@ from .models import PhotoUpload
 
 class PhotoUploadCreate(CreateView):
     model = PhotoUpload
-    template_name = 'upload_photo.html'
     form_class = PhotoUploadForm
+    template_name = 'upload_photo.html'
+
+    def get_template_names(self):
+        check_object = PhotoUpload.objects.filter(
+            user=self.request.user
+        ).order_by('-pause_upload').exists()
+
+        if check_object is False:
+            return ['upload_photo.html']
+
+        else:
+            photo_upload_object = PhotoUpload.objects.filter(
+                user=self.request.user
+            ).latest('pause_upload')
+
+            if photo_upload_object.pause_upload < timezone.now()\
+                    and photo_upload_object.pause_upload is not None:
+                return ['upload_photo.html']
+
+            else:
+                return ['counter.html']
 
     def form_valid(self, form):
-        form.instance.user = self.request.user
         check_object = PhotoUpload.objects.filter(
             user=self.request.user
         ).order_by('-pause_upload').exists()
@@ -20,14 +39,17 @@ class PhotoUploadCreate(CreateView):
         if check_object is False:
             form.instance.user = self.request.user
             return super(PhotoUploadCreate, self).form_valid(form)
-        elif check_object is True:
+
+        else:
             photo_upload_object = PhotoUpload.objects.filter(
                 user=self.request.user
             ).latest('pause_upload')
+
             if photo_upload_object.pause_upload < timezone.now()\
                     and photo_upload_object.pause_upload is not None:
                 form.instance.user = self.request.user
                 return super(PhotoUploadCreate, self).form_valid(form)
+
             else:
                 return render(self.request, 'counter.html')
 
